04f32438cc59379dac8d4d3f1a834198167e298a
RiccardoMarioBonato <Riccardomario.b@ku.th>
2026-04-12 20:53:41 +0700

    refeactor: changed title

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PortfolioOS",
+  title: "RickFolio",
   description: "A live, role-based resume platform",
 };
 
